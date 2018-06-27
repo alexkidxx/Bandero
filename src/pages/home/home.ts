9a4abbc996176034fb1ef745e1848ddbdbafca80
Alexsandro Martins <ti1@milfarma.com.br>
2018-06-26 22:13:52 -0300

    Comit inicial do Bandero

---
+++ src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { PerfilPage } from '../perfil/perfil';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+ 
+navegarPerfil(){
+  this.navCtrl.push(PerfilPage)
+}
+
+
+}
